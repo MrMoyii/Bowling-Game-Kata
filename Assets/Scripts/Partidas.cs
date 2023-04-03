b69fdf74be1d2d702a67af57748a91393d9de4e3
Omar Moyano <omarmoyano7w@gmail.com>
2023-04-03 20:03:39 -0300

    First Commit

---
+++ Assets/Scripts/Partidas.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine.TestTools;
+using UnityEngine;
+
+namespace Tests
+{
+    public class Partidas : MonoBehaviour
+    {
+        [ContextMenu("Partida")]
+        public void Partida()
+        {
+            int turnos = 10;
+            int tiradas = 2;
+
+            int puntuacionDeTurnos = 0;
+            bool strike = false;
+
+            for (int i = 0; i < turnos; i++)
+            {
+                if (strike)
+                {
+                    puntuacionDeTurnos = 10;
+                    strike = false;
+                }
+                else puntuacionDeTurnos = 0;
+
+                int bolosRestantes = 0;
+
+                if(i == 9)
+                {
+                    for (int j = 0; j < tiradas; j++)
+                    {
+                        puntuacionDeTurnos += UnityEngine.Random.Range(bolosRestantes, 10);
+                        bolosRestantes = puntuacionDeTurnos;
+                        //Spare
+                        if (puntuacionDeTurnos == 10)
+                        {
+                            puntuacionDeTurnos += UnityEngine.Random.Range(0, 10);
+                            break;
+                        }
+                        //Strike
+                        if (j == 0 && puntuacionDeTurnos == 10)
+                        {
+                            puntuacionDeTurnos += UnityEngine.Random.Range(0, 10);
+                            
+                            if(puntuacionDeTurnos != 10) bolosRestantes = puntuacionDeTurnos;
+
+                            puntuacionDeTurnos += UnityEngine.Random.Range(bolosRestantes, 10);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < tiradas; j++)
+                    {
+                        puntuacionDeTurnos += UnityEngine.Random.Range(bolosRestantes, 10);
+                        bolosRestantes = puntuacionDeTurnos;
+                        if (puntuacionDeTurnos == 10)
+                        {
+                            puntuacionDeTurnos += UnityEngine.Random.Range(0, 10);
+                            break;
+                        }
+                        if (j == 0 && puntuacionDeTurnos == 10)
+                        {
+                            //si hace "strike"
+                            strike = true;
+                            break;
+                        }
+                    }
+                }
+                Debug.Log(puntuacionDeTurnos);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/BowlingGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Tests;
+
+namespace Tests
+{
+    public class BowlingGame
+    {
+        [Test]
+        public void BowlingGameSimplePasses()
+        {
+            Partidas p = new Partidas();
+
+            p.Partida();
+
+            Assert.IsTrue(true);
+        }
+    }
+}
